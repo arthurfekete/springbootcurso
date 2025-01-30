f6cce963aa4618e44786385cc974e49f342067f9
Arthur Fekete <arthur.othero@pdcase.com.br>
2025-01-30 11:23:02 -0300

    Import

--- Projeto Spring Postgre/pagamentos/src/main/java/br/com/postgre/pagamentos/repository/PagamentosRepository.java
+++ Projeto Spring Postgre/pagamentos/src/main/java/br/com/postgre/pagamentos/repository/PagamentosRepository.java
@@ -2,7 +2,6 @@ package br.com.postgre.pagamentos.repository;
 
 import br.com.postgre.pagamentos.model.Pagamento;
 import org.springframework.data.jpa.repository.JpaRepository;
-import br.com.postgre.pagamentos.model.Pagamento;
 
 public interface PagamentosRepository extends JpaRepository<Pagamento, Long> {
 }
