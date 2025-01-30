4fa46db2d95f128286f49d7f79dd7a6cc246699b
Arthur Fekete <arthur.othero@pdcase.com.br>
2025-01-30 11:14:08 -0300

    Criação do controller

--- Projeto Spring Postgre/pagamentos/src/main/java/br/com/postgre/pagamentos/repository/PagamentosRepository.java
+++ Projeto Spring Postgre/pagamentos/src/main/java/br/com/postgre/pagamentos/repository/PagamentosRepository.java
@@ -1,5 +1,6 @@
 package br.com.postgre.pagamentos.repository;
 
+import br.com.postgre.pagamentos.model.Pagamento;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PagamentosRepository extends JpaRepository<Pagamento, Long> {
