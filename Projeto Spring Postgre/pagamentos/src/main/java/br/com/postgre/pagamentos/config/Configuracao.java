74a2879d32df8b037d42b3225304dc8dd511fd2b
kaue19santos <kaue10166@gmail.com>
2025-01-30 10:21:33 -0300

    updates

--- Projeto Spring Postgre/pagamentos/src/main/java/br/com/postgre/pagamentos/config/Configuracao.java
+++ Projeto Spring Postgre/pagamentos/src/main/java/br/com/postgre/pagamentos/config/Configuracao.java
@@ -1,11 +1,10 @@
 package br.com.postgre.pagamentos.config;
 
 
+import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class Configuracao {
-
-
 }
