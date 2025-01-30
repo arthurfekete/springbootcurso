e10105430fe7e08c7dd69300393c8a0102dd659f
Arthur Fekete <94174231+arthurfekete@users.noreply.github.com>
2025-01-30 09:28:47 -0300

    Add files via upload

---
+++ Projeto Spring Postgre/pagamentos/src/test/java/br/com/postgre/pagamentos/PagamentosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.postgre.pagamentos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PagamentosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
